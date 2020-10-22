1c6ec62cf1210bcf0d757b3a750d66f57b995638
Alex Sneddon <asneddon.dev@gmail.com>
2020-10-23 10:45:19 +1100

    Updated data generator

--- datagen.py
+++ datagen.py
@@ -1,13 +1,81 @@
 import ast
 import numpy as np
+from tensorflow import keras
 
-class DataGenerator:
-    def __init__(self, csv_file, batch_size, window_size, eval_mode, max_label_len):
-        self.csv_file = csv_file
+class DataGenerator(keras.utils.Sequence):
+    # def __init__(self, csv_file, batch_size, window_size, eval_mode, max_label_len):
+    #     self.csv_file = csv_file
+    #     self.batch_size = batch_size
+    #     self.window_size = window_size
+    #     self.eval_mode = eval_mode
+    #     self.max_label_len = max_label_len
+
+    def __init__(self, list_ids, labels, batch_size, window_size, 
+        max_label_len, shuffle=True):
+        self.list_ids = list_ids
+        self.labels = labels
         self.batch_size = batch_size
         self.window_size = window_size
-        self.eval_mode = eval_mode
         self.max_label_len = max_label_len
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        """Denotes the number of batches per epoch"""
+        return int(np.floor(len(self.list_ids) / self.batch_size))
+
+    def __getitem__(self, index):
+        """Generate one batch of data"""
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs in the batch
+        list_ids_temp = [self.list_ids[k] for k in indexes]
+
+        # Generate data for the batch
+        inputs, outputs = self.__data_generation(list_ids_temp)
+
+        return (inputs, outputs)
+
+    def on_epoch_end(self):
+        """Updates indexes after each epoch"""
+        self.indexes = np.arange(len(self.list_ids))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+    
+    def __data_generation(self, list_ids_temp):
+        """Generates data containing batch_size samples"""
+        # Initialization
+        signals = np.zeros((self.batch_size, self.window_size, 1))
+        labels = np.zeros((self.batch_size, self.max_label_len))
+        signal_length = np.ones((self.batch_size, 1)) * self.window_size
+        label_length = np.zeros((self.batch_size, 1))
+
+        # Generate data
+        for i, data_id in enumerate(list_ids_temp):
+            signal = np.load('data2/data/' + data_id)
+            signal = np.expand_dims(signal, -1)
+            # signals[i,] = signal
+            signals[i] = signal
+
+            sequence = self.labels[data_id]
+            label = self._sequence_to_label(sequence)
+            labels[i][:len(label)] = label
+            label_length[i] = len(label)
+        
+        inputs = {
+            'inputs': signals,
+            'labels': labels,
+            'input_length': signal_length,
+            'label_length': label_length
+        }
+        out = np.zeros([self.batch_size])
+        outputs = {'ctc': out}
+
+        return inputs, outputs
+
+
+    ######### OLD IMPLEMENTATION
 
     def next_batch(self):
         with open(self.csv_file, "r") as f:
--- train.py
+++ train.py
@@ -10,23 +10,28 @@ def main():
     num_val_signals = 166138
 
     # Parameters
-    batch_size = 200
+    batch_size = 2
     timesteps = 512          # Size of window during data preparation
     num_classes = 5          # A, C, G, U, Blank
     num_epochs = 2
 
-    train_csv_file = 'hek293-fold1.csv'
-    train_gen = DataGenerator(train_csv_file, batch_size, timesteps, False, max_label_length)
+    params = {'batch_size': batch_size,
+              'window_size': timesteps,
+              'max_label_len': max_label_length,
+              'shuffle': True}
 
-    val_csv_file = 'hek293-fold1.csv'
-    val_gen = DataGenerator(val_csv_file, batch_size, timesteps, False, max_label_length)
+    partition = {'train': ['HEK-1-1', 'HEK-1-2', 'HEK-1-3'], 'validation': ['HEK-1-4']}
+    labels = {'HEK-1-1': ['A', 'C'], 'HEK-1-2': ['A', 'C', 'G'], 'HEK-1-3': ['T', 'C', 'G'], 'HEK-1-4': ['C', 'C', 'T']}
+
+    training_generator = DataGenerator(partition['train'], labels, **params)
+    validation_generator = DataGenerator(partition['validation'], labels, **params)
 
     model = build_model(batch_size, timesteps, max_label_length, num_classes)
     model.compile(optimizer='adam', loss={'ctc': lambda labels, y_pred: y_pred}, run_eagerly=True)
 
     model.summary()
     model.fit(
-        x = train_gen.next_batch(),
+        x = training_generator,
         batch_size = batch_size,
         steps_per_epoch = num_train_signals // batch_size,
         epochs = num_epochs)
