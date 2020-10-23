dbb810b50af66f8c0eafadf9942959b5cbf449b1
Alex Sneddon <asneddon.dev@gmail.com>
2020-10-23 14:31:03 +1100

    Get partitions and labels from json file

--- datagen.py
+++ datagen.py
@@ -3,13 +3,6 @@ import numpy as np
 from tensorflow import keras
 
 class DataGenerator(keras.utils.Sequence):
-    # def __init__(self, csv_file, batch_size, window_size, eval_mode, max_label_len):
-    #     self.csv_file = csv_file
-    #     self.batch_size = batch_size
-    #     self.window_size = window_size
-    #     self.eval_mode = eval_mode
-    #     self.max_label_len = max_label_len
-
     def __init__(self, list_ids, labels, batch_size, window_size, 
         max_label_len, shuffle=True):
         self.list_ids = list_ids
@@ -53,7 +46,7 @@ class DataGenerator(keras.utils.Sequence):
 
         # Generate data
         for i, data_id in enumerate(list_ids_temp):
-            signal = np.load('data2/data/' + data_id)
+            signal = np.load('data2/data/' + data_id + '.npy')
             signal = np.expand_dims(signal, -1)
             # signals[i,] = signal
             signals[i] = signal
@@ -73,58 +66,7 @@ class DataGenerator(keras.utils.Sequence):
         outputs = {'ctc': out}
 
         return inputs, outputs
-
-
-    ######### OLD IMPLEMENTATION
-
-    def next_batch(self):
-        with open(self.csv_file, "r") as f:
-            while True: # Generator must always return something
-                # Initialise batch
-                signals = np.zeros((self.batch_size, self.window_size, 1))
-                labels = np.zeros((self.batch_size, self.max_label_len))
-                signal_length = np.ones((self.batch_size, 1)) * self.window_size
-                label_length = np.zeros((self.batch_size, 1))
-
-                # Get the next batch
-                for i in range(self.batch_size):
-                    line = f.readline()
-                    # Move back to beginning of file if at end
-                    if line == "":
-                        f.seek(0)
-                        line = f.readline()
-
-                        # But if we are evaluating, then we only need to go through test set once
-                        if self.eval_mode == True:
-                            break
-                    
-                    # Extract signal and label
-                    signal, label = self._extract_signal_label(line)
-                    signals[i] = signal
-                    labels[i][:len(label)] = label
-                    label_length[i] = len(label)
-
-                inputs = {
-                    'inputs': signals,
-                    'labels': labels,
-                    'input_length': signal_length,
-                    'label_length': label_length
-                }
-                out = np.zeros([self.batch_size])
-                outputs = {'ctc': out}
-
-                yield(inputs, outputs)
     
-    def _extract_signal_label(self, line):
-        signal, sequence = line.split('\t')
-        signal = ast.literal_eval(signal)
-        signal = np.expand_dims(signal, -1)
-
-        sequence = ast.literal_eval(sequence)
-        label = self._sequence_to_label(sequence)
-
-        return signal, label
-
     def _sequence_to_label(self, sequence):
         bases = ['A', 'C', 'G', 'T']
         return list(map(lambda b: bases.index(b), sequence))
--- train.py
+++ train.py
@@ -1,9 +1,14 @@
+import json
+import sys
 import tensorflow as tf
 import tcn
 from datagen import DataGenerator
 from model import build_model
 
 def main():
+    partitions_dir = sys.argv[1]
+    labels_file = sys.argv[2]
+
     # Data info
     max_label_length = 39   # Computed during data preparation
     num_train_signals = 166138 # Total number of windows in training set
@@ -20,11 +25,19 @@ def main():
               'max_label_len': max_label_length,
               'shuffle': True}
 
-    partition = {'train': ['HEK-1-1', 'HEK-1-2', 'HEK-1-3'], 'validation': ['HEK-1-4']}
-    labels = {'HEK-1-1': ['A', 'C'], 'HEK-1-2': ['A', 'C', 'G'], 'HEK-1-3': ['T', 'C', 'G'], 'HEK-1-4': ['C', 'C', 'T']}
+    partitions = []
+    for i in range(1, 11):
+        with open('{0}/partition-{1}.json'.format(partitions_dir, i), 'r') as f:
+            partitions.append(json.load(f))
 
-    training_generator = DataGenerator(partition['train'], labels, **params)
-    validation_generator = DataGenerator(partition['validation'], labels, **params)
+    with open(labels_file, 'r') as labels_f:
+        labels = json.load(labels_f)
+
+    # partition = {'train': ['HEK-1-1', 'HEK-1-2', 'HEK-1-3'], 'validation': ['HEK-1-4']}
+    # labels = {'HEK-1-1': ['A', 'C'], 'HEK-1-2': ['A', 'C', 'G'], 'HEK-1-3': ['T', 'C', 'G'], 'HEK-1-4': ['C', 'C', 'T']}
+
+    training_generator = DataGenerator(partitions[0]['train'], labels, **params)
+    validation_generator = DataGenerator(partitions[0]['validation'], labels, **params)
 
     model = build_model(batch_size, timesteps, max_label_length, num_classes)
     model.compile(optimizer='adam', loss={'ctc': lambda labels, y_pred: y_pred}, run_eagerly=True)
