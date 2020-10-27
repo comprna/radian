a4e6a49b6768e4c46d8a9506643503ce008d0876
Alex Sneddon <asneddon.dev@gmail.com>
2020-10-27 16:56:36 +1100

    Added config file for parameters, clean-up

--- model.py
+++ model.py
@@ -1,44 +1,44 @@
-import tensorflow as tf
-import tcn
-import functools
+from tcn import TCN
+from tensorflow.keras.layers import Dense, Activation, Lambda
+from tensorflow.keras import Input, Model, backend
 
 def ctc_loss_lambda(args):
     """
     This function is required because Keras currently doesn't support
     loss functions with additional parameters so it needs to be
     implemented in a lambda layer.
-
-    y_true:  Tensor (samples, max_string_length) containing the truth 
-        labels.
-        --> samples = batch_size
-        --> These labels should be padded to the length of the longest
-            label in the batch.  In the "padded slots" any value can 
-            be used, because ctc_batch_cost internally combines
-            y_true with label_length to produce a sparce tensor, so any
-            values in slots beyond label_length will be ignored.
-    y_pred:	 Tensor (samples, time_steps, num_categories) containing 
-        the prediction, or output of the softmax.
-    input_length:  Tensor (samples, 1) containing the sequence length 
-        for each batch item in y_pred.
-    label_length:  Tensor (samples, 1) containing the sequence length 
-        for each batch item in y_true.
     """
     y_pred, labels, input_length, label_length = args
-    return tf.keras.backend.ctc_batch_cost(labels, y_pred, input_length, label_length)
+    return backend.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
-def build_model(batch_size, timesteps, max_label_len, num_classes):
-    input_shape = (timesteps, 1)
+def build_model(config, max_label_len):
+    c = config
+    input_shape = (c.timesteps, 1)
     
-    inputs = tf.keras.Input(shape=input_shape, name="inputs") # (None, 512, 1)
+    inputs = Input(shape=input_shape, name="inputs") # (None, 512, 1)
 
-    inner = tcn.TCN(return_sequences=True)(inputs) # (None, 512, 64)
-    inner = tf.keras.layers.Dense(num_classes)(inner) # (None, 512, 5)
-    y_pred = tf.keras.layers.Activation('softmax')(inner) # (None, 512, 5)
+    params = {'nb_filters': c.tcn.nb_filters,
+              'kernel_size': c.tcn.kernel_size,
+              'nb_stacks': c.tcn.nb_stacks,
+              'dilations': c.tcn.dilations,
+              'padding': c.tcn.padding,
+              'use_skip_connections': c.tcn.use_skip_connections,
+              'dropout_rate': c.tcn.dropout_rate,
+              'return_sequences': c.tcn.return_sequences,
+              'activation': c.tcn.activation,
+              'kernel_initializer': c.tcn.kernel_initializer,
+              'use_batch_norm': c.tcn.use_batch_norm}
 
-    labels = tf.keras.Input(shape=(max_label_len,), name="labels") # (None, 39)
-    input_length = tf.keras.Input(shape=[1],name="input_length") # (None, 1)
-    label_length = tf.keras.Input(shape=[1],name="label_length") # (None, 1)
+    inner = TCN(**params)(inputs)   # (None, 512, 64)
+    inner = Dense(c.relu_units)(inner) # (None, 512, 5)
+    inner = Activation('relu')(inner)
+    inner = Dense(c.softmax_units)(inner) # (None, 512, 5)
+    y_pred = Activation('softmax')(inner) # (None, 512, 5)
 
-    loss_out = tf.keras.layers.Lambda(ctc_loss_lambda, output_shape=(1,), name='ctc')((y_pred, labels, input_length, label_length))
+    labels = Input(shape=(max_label_len,), name="labels") # (None, 39)
+    input_length = Input(shape=[1],name="input_length") # (None, 1)
+    label_length = Input(shape=[1],name="label_length") # (None, 1)
+
+    loss_out = Lambda(ctc_loss_lambda, output_shape=(1,), name='ctc')((y_pred, labels, input_length, label_length))
     
-    return  tf.keras.Model(inputs=[inputs, labels, input_length, label_length], outputs=[loss_out])
+    return  Model(inputs=[inputs, labels, input_length, label_length], outputs=[loss_out])
--- train.py
+++ train.py
@@ -1,78 +1,91 @@
 import json
 import sys
-import tensorflow as tf
-import tcn
+import yaml
+from attrdict import AttrDict
 from datagen import DataGenerator
 from model import build_model
+from tensorflow import Variable
+from tensorflow.keras.optimizers.schedules import PiecewiseConstantDecay
+from tensorflow.keras.optimizers import Adam
 
-def compute_max_label_len(labels):
+def compute_max_length(labels):
     max_len = 0
-    for label in labels.values():
+    for label in labels:
         if len(label) > max_len:
             max_len = len(label)
     return max_len
 
-def main():
+def get_config(filepath):
+    with open(filepath) as config_file:
+        return AttrDict(yaml.load(config_file, Loader=yaml.Loader))
+
+def get_partitions(config, dir):
+    partitions = []
+    for i in range(1, config.train.n_folds + 1):
+        with open('{0}/partition-{1}.json'.format(dir, i), 'r') as f:
+            partitions.append(json.load(f))
+    return partitions
+
+def get_labels(labels_file):
+    with open(labels_file, 'r') as labels_f:
+        return json.load(labels_f)
+
+def get_optimizer(config):
+    step = Variable(0, trainable = False)
+    boundaries = [int(config.max_steps * bound) for bound in config.boundaries]
+    values = [config.init_rate * decay for decay in config.decays]
+    learning_rate_fn = PiecewiseConstantDecay(boundaries, values)
+    return Adam(learning_rate=learning_rate_fn(step))
+
+def train():
     partitions_dir = '/home/alex/OneDrive/phd-project/singleton-dataset-generation/dRNA/3_8_NNInputs'
     labels_file = '/home/alex/OneDrive/phd-project/singleton-dataset-generation/dRNA/3_8_NNInputs/labels.json'
 
-    # Parameters
-    batch_size = 256
-    timesteps = 512          # Size of window during data preparation
-    num_classes = 5          # A, C, G, U, Blank
-    num_epochs = 1
+    # Load params
+    c = get_config('config.yaml')
 
-    # Train/val data
-    partitions = []
-    for i in range(1, 11):
-        with open('{0}/partition-{1}.json'.format(partitions_dir, i), 'r') as f:
-            partitions.append(json.load(f))
-
-    # Labels
-    with open(labels_file, 'r') as labels_f:
-        labels = json.load(labels_f)
-    max_label_length = compute_max_label_len(labels)
-
-    params = {'batch_size': batch_size,
-            'window_size': timesteps,
-            'max_label_len': max_label_length,
-            'shuffle': True}
+    # Prepare data generators
+    labels = get_labels(labels_file)
+    max_label_length = compute_max_length(labels.values())
+    partitions = get_partitions(c, partitions_dir)
+    params = {'batch_size': c.train.batch_size,
+              'window_size': c.data.window_size,
+              'max_label_len': max_label_length,
+              'shuffle': True}
 
     # Train using k-fold CV
     cv_scores = []
     for i, partition in enumerate(partitions):
         print("Training model {0}...".format(i))
 
-        training_generator = DataGenerator(partition['train'], labels, **params)
-        validation_generator = DataGenerator(partition['val'], labels, **params)
+        train_generator = DataGenerator(partition['train'], labels, **params)
+        val_generator = DataGenerator(partition['val'], labels, **params)
 
-        model = build_model(batch_size, timesteps, max_label_length, num_classes)
-        model.compile(optimizer='adam', 
-                      loss={'ctc': lambda labels, y_pred: y_pred})
-        # For debugging:
-        # model.compile(optimizer='adam', 
-        #               loss={'ctc': lambda labels, y_pred: y_pred},
-        #               run_eagerly=True)
+        n_train_signals = len(partition['train'])
+        n_val_signals = len(partition['val'])
 
-        num_train_signals = len(partition['train'])
-        num_val_signals = len(partition['val'])
+        model = build_model(c.model, max_label_length)
+        optimizer = get_optimizer(c.train.opt)
+        model.compile(optimizer = optimizer,
+                      loss = {'ctc': lambda labels, y_pred: y_pred})
 
         model.summary()
         model.fit(
-            x = training_generator,
-            validation_data=validation_generator,
-            validation_steps = num_val_signals // batch_size,
-            # validation_freq = 2,
-            batch_size = batch_size,
-            steps_per_epoch = num_train_signals // batch_size,
-            epochs = num_epochs)
+            x = train_generator,
+            validation_data=val_generator,
+            validation_steps = n_val_signals // c.train.batch_size,
+            validation_freq = c.train.val_freq,
+            steps_per_epoch = n_train_signals // c.train.batch_size,
+            epochs = c.train.n_epochs)
+            # use_multiprocessing = True,
+            # workers = 2)
         
         score = model.evaluate(
-            x = validation_generator,
-            steps = num_val_signals // batch_size)
+            x = val_generator,
+            steps = n_val_signals // c.train.batch_size)
         print("Model {0} scores: Loss = {1}".format(i, score))
         cv_scores.append(score)
     print(cv_scores)
 
 if __name__ == "__main__":
-    main()
+    train()
