5a9f26f35351ac59f69080be7e969c32a5b2cbae
Alex Sneddon <asneddon.dev@gmail.com>
2020-10-29 15:31:32 +1100

    WIP: Add checkpointing

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import sys
 import yaml
@@ -7,6 +8,8 @@ from model import build_model
 from tensorflow import Variable
 from tensorflow.keras.optimizers.schedules import PiecewiseConstantDecay
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import load_model
 
 def compute_max_length(labels):
     max_len = 0
@@ -37,7 +40,7 @@ def get_optimizer(config):
     learning_rate_fn = PiecewiseConstantDecay(boundaries, values)
     return Adam(learning_rate=learning_rate_fn(step))
 
-def train():
+def train(checkpoint, epoch_to_resume):
     partitions_dir = '/home/alex/OneDrive/phd-project/singleton-dataset-generation/dRNA/3_8_NNInputs'
     labels_file = '/home/alex/OneDrive/phd-project/singleton-dataset-generation/dRNA/3_8_NNInputs/labels.json'
 
@@ -64,10 +67,22 @@ def train():
         n_train_signals = len(partition['train'])
         n_val_signals = len(partition['val'])
 
-        model = build_model(c.model, max_label_length)
-        optimizer = get_optimizer(c.train.opt)
-        model.compile(optimizer = optimizer,
-                      loss = {'ctc': lambda labels, y_pred: y_pred})
+        if checkpoint is not None:
+            model = load_model(checkpoint)
+            initial_epoch = epoch_to_resume
+        else:
+            model = build_model(c.model, max_label_length)
+            optimizer = get_optimizer(c.train.opt)
+            initial_epoch = 0
+            model.compile(optimizer = optimizer,
+                        loss = {'ctc': lambda labels, y_pred: y_pred})
+
+        checkpoint_path = "model-{epoch:02d}-{val_loss:.2f}.h5"
+        checkpoint = ModelCheckpoint(checkpoint_path, 
+                                     monitor="val_loss",
+                                     verbose=1,
+                                     mode="min")
+        callbacks_list = [checkpoint]
 
         model.summary()
         model.fit(
@@ -76,10 +91,12 @@ def train():
             validation_steps = n_val_signals // c.train.batch_size,
             validation_freq = c.train.val_freq,
             steps_per_epoch = n_train_signals // c.train.batch_size,
-            epochs = c.train.n_epochs)
+            epochs = c.train.n_epochs,
+            initial_epoch = initial_epoch,
+            callbacks = callbacks_list)
             # use_multiprocessing = True,
             # workers = 2)
-        
+
         score = model.evaluate(
             x = val_generator,
             steps = n_val_signals // c.train.batch_size)
@@ -87,7 +104,15 @@ def train():
         cv_scores.append(score)
     print(cv_scores)
 
-    # TODO(save model)
-
 if __name__ == "__main__":
-    train()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--checkpoint", help="path to weights checkpoint to load")
+    parser.add_argument("-e", "--initial_epoch", help="epoch to resume training at")
+    args = parser.parse_args()
+
+    if args.checkpoint is not None:
+        assert args.initial_epoch is not None
+    
+    # TODO(alex): Keep track of which partition we are up to!
+
+    train(args.checkpoint, args.initial_epoch)
