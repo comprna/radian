ff370313f1760a707e887801a1b2c32b9e4aba52
Alex Sneddon <asneddon.dev@gmail.com>
2020-11-03 11:54:01 +1100

    Updated training and config for first gadi run

--- train.py
+++ train.py
@@ -105,9 +105,9 @@ if __name__ == "__main__":
         assert args.initial_epoch is not None
         assert args.partition is not None
 
-    # physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    # assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-    # config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+    config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
     train(args.checkpoint, 
           args.initial_epoch, 
