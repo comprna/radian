cf9c98a2c2d56ac859f387557e0d16ec9ae87281
Alex Sneddon <asneddon.dev@gmail.com>
2020-11-06 16:31:36 +1100

    Test checkpointing

--- train.py
+++ train.py
@@ -155,8 +155,8 @@ if __name__ == "__main__":
     # sess = tf.compat.v1.Session(config=config)
     # tf.compat.v1.keras.backend.set_session(sess)
 
-    # train(args.shards_dir,
-    #       args.checkpoint, 
-    #       int(args.initial_epoch), 
-    #       args.config_file)
-    train('/mnt/sda/singleton-dataset-generation/dRNA/3_8_NNInputs/tfrecord_approach/shards', 'model-06-27.38.h5', 7, 'config.yaml')
+    train(args.shards_dir,
+          args.checkpoint, 
+          int(args.initial_epoch), 
+          args.config_file)
+    # train('/mnt/sda/singleton-dataset-generation/dRNA/3_8_NNInputs/tfrecord_approach/shards', 'model-06-27.38.h5', 7, 'config.yaml')
