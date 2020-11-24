57105c14c2811571734f12ade3e26e66d2841c05
Alex Sneddon <asneddon.dev@gmail.com>
2020-11-25 10:57:20 +1100

    Add edit distance to test

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import numpy as np
 from tensorflow.io.gfile import glob
 from tensorflow.keras.models import load_model
 from tensorflow.keras import backend as K
+from textdistance import levenshtein
 
 from data import get_dataset
 from model import get_prediction_model
@@ -21,38 +22,49 @@ def main():
     saved_filepath = '/g/data/xc17/Eyras/alex/rna-basecaller/train-16/model-389.h5'
     model = get_prediction_model(saved_filepath, config)
 
-    i = 1
     for batch in test_dataset:
-        if i > 10:
-            break # Just go through 10 batches
         inputs = batch[0]["inputs"]
         labels = batch[0]["labels"]
-        input_length = batch[0]["input_length"]
-        print(inputs)
-        print(inputs.shape) # (256, 512)
+        input_lengths = batch[0]["input_length"]
+        label_lengths = batch[0]["label_length"]
 
         # Pass test data into network
         softmax_out = model.predict(inputs)
-        print(softmax_out.shape) # (256, 512, 5)
 
         # CTC decoding of network outputs
-        prediction = K.ctc_decode(softmax_out, input_length, greedy=True, beam_width=100, top_paths=1)
-        print(prediction)
+        prediction = K.ctc_decode(softmax_out, input_lengths, greedy=True, beam_width=100, top_paths=1)
         prediction = K.get_value(prediction[0][0])
-        print(prediction)
 
         for i, p in enumerate(prediction):
             signal = inputs[i]
             label = labels[i]
+            label_length = label_lengths[i]
             y_pred = softmax_out[i]
-            # plt.plot(signal)
-            # plt.show()
             print("Label: {0}".format(label))
+            print("Label length: {0}".format(label_length))
+            label = _label_to_sequence(label, label_length)
+            print("Label after format: {0}".format(label))
+
             print("Predicted sequence: {0}".format(p))
+            p = _label_to_sequence(p, label_length)
+            print("Predicted sequence after format: {0}".format(p))
+
             print("Softmax output: {0}".format(y_pred))
             print("\n\n\n")
-        
-        i += 1
+            edit_dist = levenshtein.normalized_distance(label, p)
+            print("Edit distance: {0}".format(edit_dist))
+            break
+
+def _label_to_sequence(label, label_length):
+    print(label)
+    label = label[:label_length]
+    print(label)
+    bases = ['A', 'C', 'G', 'T']
+    label = list(map(lambda b: bases[b], label))
+    print(label)
+    return "".join(label)
+
+
 
     # [OPTIONAL] Assemble into reads
 
