640b49e974a18a75f9896248423c8340059c7131
Alex Sneddon <asneddon.dev@gmail.com>
2021-01-29 12:14:55 +1100

    Test train-1

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def main():
     test_files = glob("/mnt/sda/singleton-dataset-generation/dRNA/4_8_NNInputs/0_2_CreateTFRecords/2_WriteTFRecords/shards/val/*.tfrecords")
     test_dataset = get_dataset(test_files, config.train.batch_size, val=True)
 
-    saved_filepath = '/home/alex/Documents/tmp/train-9/model-01.h5'
+    saved_filepath = '/mnt/sda/rna-basecaller/experiments/4_8_NNInputs/train-1/model-01.h5'
     model = get_prediction_model(saved_filepath, config)
 
     # TODO: Assemble into reads
