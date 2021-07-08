a79b40922920b2f66f936e6ab7a6e2b68530c929
Alex Sneddon <asneddon.dev@gmail.com>
2021-07-08 15:20:53 +1000

    Set up local testing of rna model.

--- test_rna_model.py
+++ test_rna_model.py
@@ -12,37 +12,44 @@ from rna_model import get_rna_prediction_model
 from utilities import get_config, setup_local
 
 def main():
+    # Run locally or on gadi
+
+    local_dir = '/mnt/sda/rna-basecaller/experiments/with-rna-model/global/all_val/copied_files'
+    gadi_dir = '/g/data/xc17/Eyras/alex/working/with-rna-model/global/all_val/copied_files'
+
+    base_dir = local_dir
+    setup_local()
+
     # Load config
 
-    gadi_dir = '/g/data/xc17/Eyras/alex/working/test_decoding' # TODO: CL param
-    r_config_file = f"{gadi_dir}/r-config-38.yaml"  # TODO: CL param
+    r_config_file = f"{base_dir}/tmp/r-config-38.yaml"  # TODO: CL param
     r_config = get_config(r_config_file)
 
-    d_config_file = '/mnt/sda/rna-basecaller/experiments/global-decoding/train-3-37/val_test/10_AddRnaModel/all_val/d-config-1.yaml' # TODO: CL param
+    d_config_file = '/mnt/sda/rna-basecaller/experiments/with-rna-model/global/all_val/decode-1/d-config-1.yaml' # TODO: CL param
     d_config = get_config(d_config_file)
 
     # Load RNA model
 
-    r_model_file = f"{gadi_dir}/r-train-38-model-01.h5" # TODO: CL param
+    r_model_file = f"{base_dir}/tmp/r-train-38-model-01.h5" # TODO: CL param
     r_model = get_rna_prediction_model(r_model_file, r_config)
 
     # Load read IDs
 
-    id_file = f"{gadi_dir}/heart_read_ids.npy"
+    id_file = f"{base_dir}/heart_read_ids.npy"
     with open(id_file, "rb") as f:
         read_ids = np.load(f, allow_pickle=True)
     print(len(read_ids))
 
     # Load ground truth sequences
 
-    gt_file = f"{gadi_dir}/heart_read_gt_labels.json"
+    gt_file = f"{base_dir}/heart_read_gt_labels.json"
     with open(gt_file, "r") as f:
         gts = json.load(f)
 
     # Load global softmaxes
 
-    gs_file = f"{gadi_dir}/heart_global_softmaxes_all.npy"
-    with open(gs_file, "r") as f:
+    gs_file = f"{base_dir}/heart_global_softmaxes_all.npy"
+    with open(gs_file, "rb") as f:
         global_softmaxes = np.load(f, allow_pickle=True)
 
     # Decode global softmax with RNA model
@@ -50,8 +57,9 @@ def main():
     classes = 'ACGT'
     eds = []
     for i, softmax in enumerate(global_softmaxes):
-        # pred, _ = ctcBeamSearch(softmax, classes, None, None)
-        pred, _ = ctcBeamSearch(softmax, classes, r_model, None, lm_factor=d_config.lm_factor)
+        pred, _ = ctcBeamSearch(softmax, classes, None, None)
+        # pred, _ = ctcBeamSearch(softmax, classes, r_model, None, lm_factor=d_config.lm_factor)
+        print(pred)
         ed = levenshtein.normalized_distance(gts[read_ids[i]], pred)
         eds.append(ed)
         print(f"{read_ids[i]}\t{gts[read_ids[i]]}\t{pred}\t{ed}")
