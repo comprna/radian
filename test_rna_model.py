d6213a5253ed17288c06d2087a403d9a5750b736
Alex Sneddon <asneddon.dev@gmail.com>
2021-07-08 15:30:01 +1000

    Add lm_factor as config param.

--- test_rna_model.py
+++ test_rna_model.py
@@ -59,8 +59,7 @@ def main():
     if d_config.use_rna_model == False:
         r_model = None
     for i, softmax in enumerate(global_softmaxes):
-        pred, _ = ctcBeamSearch(softmax, classes, r_model, None)
-        # pred, _ = ctcBeamSearch(softmax, classes, r_model, None, lm_factor=d_config.lm_factor)
+        pred, _ = ctcBeamSearch(softmax, classes, r_model, None, lm_factor=d_config.lm_factor)
         print(pred)
         ed = levenshtein.normalized_distance(gts[read_ids[i]], pred)
         eds.append(ed)
