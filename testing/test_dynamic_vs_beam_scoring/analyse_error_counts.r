ece7376d27276a0e7bb4d68b0a8254c9e48d2493
Alex Sneddon <asneddon.dev@gmail.com>
2021-09-01 12:32:07 +1000

    Move text files to /mnt/sda and move scripts to testing folder.

---
+++ testing/test_dynamic_vs_beam_scoring/analyse_error_counts.r
@@ -0,0 +1,46 @@
+library(dplyr)
+library(glue)
+library(magrittr)
+library(readr)
+
+# Load results into tibble
+
+filepath <- 'error_counts_decode-51.tsv'
+
+filepath %>%
+  read_tsv() %>%
+  print() ->
+ds
+
+# Analyse error %
+
+ds %<>%
+  mutate(tot_error = n_sub + n_ins + n_del,
+         tot_pcent = tot_error / gt_length * 100,
+         sub_pcent = n_sub / gt_length * 100,
+         ins_pcent = n_ins / gt_length * 100,
+         del_pcent = n_del / gt_length * 100,
+         hdel_pcent = n_hdel / n_del * 100,
+         ctsub_pcent = n_ctsub / n_sub * 100,
+         cgsub_pcent = n_cgsub / n_sub * 100,
+         casub_pcent = n_casub / n_sub * 100,
+         gasub_pcent = n_gasub / n_sub * 100,
+         gtsub_pcent = n_gtsub / n_sub * 100,
+         atsub_pcent = n_atsub / n_sub * 100,
+         )
+
+print(mean(ds[['n_alignments']]))
+print(mean(ds[['tot_pcent']]))
+print(mean(ds[['sub_pcent']]))
+print(mean(ds[['ins_pcent']]))
+print(mean(ds[['del_pcent']]))
+print(mean(ds[['hdel_pcent']]))
+print(mean(ds[['ctsub_pcent']], na.rm=TRUE))
+print(mean(ds[['cgsub_pcent']], na.rm=TRUE))
+print(mean(ds[['casub_pcent']], na.rm=TRUE))
+print(mean(ds[['gasub_pcent']], na.rm=TRUE))
+print(mean(ds[['gtsub_pcent']], na.rm=TRUE))
+print(mean(ds[['atsub_pcent']], na.rm=TRUE))
+
+
+
