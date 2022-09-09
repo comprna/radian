66d0e1a91541030e21d01f33295003564233b3d0
Alex Sneddon <asneddon.dev@gmail.com>
2022-09-09 15:58:52 +1000

    Test basecalling on gadi.

--- basecall.py
+++ basecall.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import sys
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -15,11 +16,13 @@ from utilities import get_config, setup_local
 
 def main():
     # Comment out if running on gadi
-    setup_local()
-    
+    # setup_local()
+
     # Data directories
-    fast5_dir = "/mnt/sda/rna-basecaller/benchmarking/0_TestData/heart" # Single or multi fast5s
-    fastq_dir = 'fastq'
+    # fast5_dir = "/mnt/sda/rna-basecaller/benchmarking/0_TestData/heart" # Single or multi fast5s
+    # fastq_dir = 'fastq'
+    fast5_dir = sys.argv[1]
+    fastq_dir = sys.argv[2]
 
     # Preprocessing parameters
     outlier_z_score = 4
@@ -28,16 +31,23 @@ def main():
     batch_size = 32
 
     # Model files
-    rna_model_file = "kmer_model/transcripts-6mer-rna-model.json"
-    sig_config_file = '/mnt/sda/rna-basecaller/benchmarking/2_SigModel/s-config-3.yaml'
-    sig_model_file = '/mnt/sda/rna-basecaller/benchmarking/2_SigModel/s-model-3-10.h5'
+    # rna_model_file = "kmer_model/transcripts-6mer-rna-model.json"
+    # sig_config_file = '/mnt/sda/rna-basecaller/benchmarking/2_SigModel/s-config-3.yaml'
+    # sig_model_file = '/mnt/sda/rna-basecaller/benchmarking/2_SigModel/s-model-3-10.h5'
+    rna_model_file = sys.argv[3]
+    sig_config_file = sys.argv[4]
+    sig_model_file = sys.argv[5]
 
     # Decoding parameters
-    decode = "local"
     beam_width = 6
-    s_threshold = 0.5
-    r_threshold = 0.5
-    context_len = 5
+    # decode = "local"
+    # s_threshold = 0.5
+    # r_threshold = 0.5
+    # context_len = 5
+    decode = sys.argv[6]
+    s_threshold = float(sys.argv[7])
+    r_threshold = float(sys.argv[8])
+    context_len = int(sys.argv[9])
 
     # Load RNA model
     with open(rna_model_file, "r") as f:
@@ -60,9 +70,14 @@ def main():
     fastq = open(f"{fastq_dir}/reads-{fastq_n}.fastq", "w")
 
     # Basecall each read in fast5 directory
+    n = 0
     for fast5_filepath in Path(fast5_dir).rglob('*.fast5'):
         with get_fast5_file(fast5_filepath, 'r') as fast5:
             for read in fast5.get_reads(): 
+                if n >= 5:
+                    break
+                n += 1
+
                 # Preprocess read
                 raw_signal = read.get_raw_data()
                 norm_signal = mad_normalise(raw_signal, outlier_z_score)
