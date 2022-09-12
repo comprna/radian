d1113c657ab8a030b03274f0b54854e3492683b8
Alex Sneddon <asneddon.dev@gmail.com>
2022-09-12 13:24:51 +1000

    Accuracy calcs.

--- accuracy.py
+++ accuracy.py
@@ -1,73 +1,18 @@
 import re
 import sys
+
+import numpy as np
 import pysam
-
-
-def parse_cs_tag(tag, refseq, readseq, debug=False):
-    ret = ""
-    match = 0
-    mismatch = 0
-    deletions = 0
-    insertions = 0
-    refpos = 0
-    readpos = 0
-    refstr = ""
-    readstr = ""
-    p = re.findall(r":([0-9]+)|(\*[a-z][a-z])|(=[A-Za-z]+)|(\+[A-Za-z]+)|(\-[A-Za-z]+)", tag)
-    for i, x in enumerate(p):
-        #print(x)
-        if len(x[0]):
-            q = int(x[0])
-            if debug: print("match:", i, q)
-            match+= int(q)
-            refstr+=refseq[refpos:refpos+q]
-            refpos+=q
-            readstr+=readseq[readpos:readpos+q]
-            readpos+=q
-            ret+= "|" * q
-        if len(x[1]):
-            q = int((len(x[1])-1)/2)
-            if debug: print("mismatch:", i, q)
-            mismatch+= q
-            refstr+=refseq[refpos:refpos+q]
-            refpos+=q
-            readstr+=readseq[readpos:readpos+q]
-            readpos+=q
-            ret+= "*" * q
-        if len(x[2]):
-            if debug: print("FAIL")
-        if len(x[3]):
-            q = len(x[3])-1
-            if debug: print("insertion:", i, q, x[3])
-            insertions+=q
-            refstr+="-"*q
-            readstr+=readseq[readpos:readpos+q]
-            readpos+=q
-            ret+= " " * q
-        if len(x[4]):
-            q = len(x[4])-1
-            if debug: print("deletion:", i, q)
-            deletions+=q
-            refstr+=refseq[refpos:refpos+q]
-            refpos+=q
-            readstr+="-"*q
-            ret+= " " * q
-
-    return match, insertions, deletions, mismatch
 
 
 def main():
     # SAM file to parse
-    # sam_file = sys.argv[1]
-    sam_file = "/home/alex/Documents/tmp/ngram-1-aln.sam"
-    ref_file = "/home/alex/OneDrive/phd-project/basecaller-data/dRNA/1_DivideTrainTestFast5s/0_0_RawData/gencode.v34.transcripts.fa"
+    sam_file = sys.argv[1]
+    # sam_file = "/home/alex/Documents/tmp/ngram-1-aln.sam"
     out_file = sam_file.replace(".sam", ".tsv")
 
-    # Record stats
+    # Store stats for all reads
     stats = []
-
-    # Load reference file
-    ref_file = pysam.FastaFile(ref_file)
 
     # Parse SAM file
     sam_file = pysam.AlignmentFile(sam_file, "r")
@@ -80,53 +25,54 @@ def main():
                 n_unmapped += 1
                 continue
 
-            # Get reference sequence
-            ref_seq = ref_file.fetch(read.reference_name, read.reference_start, read.reference_end)
-            
             if not read.seq:
-                print("TODO: NO QUERY SEQ")
+                print("ERROR: NO QUERY SEQ")
+                continue
 
             # Extract relevant info from alignment record
             read_id = read.qname
             ref_name = read.reference_name.split("|")
             transcript = ref_name[0]
-            seq = read.seq[read.query_alignment_start:read.query_alignment_end]
-            
-            # RODAN version
-            cs_tag = read.get_tag("cs")
-            n_match, n_ins, n_del, n_sub = parse_cs_tag(cs_tag, ref_seq, seq)
-        
-            # My version
-            my_match = 0
-            my_ins = 0
-            my_del = 0
-            my_sub = 0
+
+            # Calculate metrics
+            n_match = 0
+            n_ins = 0
+            n_del = 0
+            n_sub = 0
             for char in read.cigar:
                 op = char[0]
                 count = char[1]
-                if op == 0:
-                    my_match += count
-                elif op == 1:
-                    my_ins += count
-                elif op == 2:
-                    my_del += count
+                if op == 0:            # M (0): Matches + substitutions
+                    n_match += count
+                elif op == 1:          # I (1): Insertions
+                    n_ins += count
+                elif op == 2:          # D (2): Deletions
+                    n_del += count
             
-            nm = read.get_tag("NM")
-            my_sub = nm - my_ins - my_del
-            my_match -= my_sub
+            nm = read.get_tag("NM")    # NM: Ins + del + sub
+            n_sub = nm - n_ins - n_del
+            n_match -= n_sub
 
-            print(f"Match: {my_match}\t{n_match}")
-            print(f"Ins: {my_ins}\t{n_ins}")
-            print(f"Del: {my_del}\t{n_del}")
-            print(f"Sub: {my_sub}\t{n_sub}")
+            # Write raw stats for downstream analysis
+            out.write(f"{read_id}\t{transcript}\t{n_match}\t{n_ins}\t{n_del}\t{n_sub}")
 
-            
+            # Compute accuracy and error percentages
+            acc = n_match / (n_match + nm) * 100
+            p_ins = n_ins / (n_match + nm) * 100
+            p_del = n_del / (n_match + nm) * 100
+            p_sub = n_sub / (n_match + nm) * 100
+            p_err = (n_ins + n_del + n_sub) / (n_match + nm) * 100
+            stats.append([acc, p_ins, p_del, p_sub, p_err])
 
-            # stats.append([n_match, n_ins, n_del, n_sub])
-            # out.write(f"{read_id}\t{transcript}\t{n_match}\t{n_ins}\t{n_del}\t{n_sub}")
-    
-    for read in stats:
-        print(read)
+    # Print metrics
+    stats = np.asarray(stats)
+    print(f"N unmapped reads: {n_unmapped}")
+    print(f"N mapped reads: {len(stats)}")
+    print(f"Accuracy\tMEDIAN: {np.median(stats[:,0]):.2f}\tMEAN: {np.mean(stats[:,0]):.2f}")
+    print(f"Insertions\tMEDIAN: {np.median(stats[:,1]):.2f}\tMEAN: {np.mean(stats[:,1]):.2f}")
+    print(f"Deletions\tMEDIAN: {np.median(stats[:,2]):.2f}\tMEAN: {np.mean(stats[:,2]):.2f}")
+    print(f"Substitutions\tMEDIAN: {np.median(stats[:,3]):.2f}\tMEAN: {np.mean(stats[:,3]):.2f}\n")
+    print(f"Total error\tMEDIAN: {np.median(stats[:,4]):.2f}\tMEAN: {np.mean(stats[:,4]):.2f}\n")
 
 
 if __name__ == "__main__":
